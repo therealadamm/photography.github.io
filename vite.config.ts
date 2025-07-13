8c0c40a0d5df5af6bd02e220fc51907fcd99b610
Adam Fahmi Bin Mohd Adnan <147355452+therealadamm@users.noreply.github.com>
2025-07-13 18:27:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/photography.github.io/", // ✅ This is what makes GitHub Pages work
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
