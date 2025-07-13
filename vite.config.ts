f18928e35ed9b227bcd292eed3b3aceff0c5d0fe
Adam Fahmi Bin Mohd Adnan <147355452+therealadamm@users.noreply.github.com>
2025-07-13 18:29:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/photography.github.io/", // ✅ This is what makes GitHub Pages work
